898643d83966f47236db726ec1f82d743e6ad01b
Khawaja Tayyab <khawaja.tayyab@alchemative.com>
2025-04-21 11:03:57 +0500

    secret keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "Simply Wish List"
-  config.api_key = "0c5ee7672aa037e396b19c97ff15ea5e"
-  config.secret = "b5b4a0bfd38ed06a74cf01c955102a0b"
+  config.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  config.secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   config.scope = "read_orders, read_products, read_customers, read_products"
   config.embedded_app = true
 end
